bfc3499fdc8315b6f8ff9a48471417d4b609e6bd
Simon Zheng <simz97@outlook.com>
2018-03-07 17:32:27 -0500

    HW2 continuu

--- hw2/Script.sql
+++ hw2/Script.sql
@@ -50,7 +50,158 @@ ORDER BY
 	C.ccode
 ;
 
--- .
+-- 4.5.
+SELECT
+	C.ccode, C.credits
+FROM
+	course C
+WHERE
+	C.dept = 'computer science'
+	AND EXISTS(
+		SELECT CO.ccode
+		FROM courseoffer CO
+		WHERE CO.term = 'winter 2018' AND CO.ccode = C.ccode )
+ORDER BY
+	C.ccode
+;
+
+-- 5.
+SELECT
+	DISTINCT C.ccode, C.credits
+FROM
+	course C, enroll E
+WHERE
+	C.ccode = E.ccode AND E.sid = 12345678
+ORDER BY
+	C.ccode
+;
+
+-- 6.
+SELECT
+	DISTINCT C.ccode, C.credits
+FROM
+	course C, enroll E
+WHERE
+	C.ccode = E.ccode AND E.sid = 12345678 AND E.term = 'winter 2018'
+ORDER BY
+	C.ccode
+;
+
+-- 7.
+SELECT
+	DISTINCT CO.ccode
+FROM
+	courseoffer CO
+WHERE
+	CO.term = 'winter 2017' AND CO.ccode NOT IN(
+		SELECT ccode FROM courseoffer WHERE term = 'winter 2018' )
+ORDER BY
+	CO.ccode
+;
+
+-- 8.
+SELECT
+	DISTINCT C.ccode, C.credits
+FROM
+	course C, enroll E
+WHERE
+	C.ccode = E.ccode AND E.term = 'winter 2018' AND E.sid IN(
+		SELECT sid FROM enroll WHERE sid = 12345678 AND sid = 12345679 )
+ORDER BY
+	C.ccode
+;
+/*
+-- 9.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 10.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 11.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 12.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 13.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 14.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 15.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 16.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 17.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 18.
+SELECT
+	
+FROM
+	
+WHERE
+	
+;
+
+-- 19.
 SELECT
 	S.rating, AVG( S.age )
 FROM
@@ -63,3 +214,17 @@ ORDER BY
 	S.rating DESC
 ;
 
+-- 20.
+SELECT
+	
+FROM
+	
+WHERE
+	
+GROUP BY
+	
+ORDER BY
+	
+;
+
+*/
