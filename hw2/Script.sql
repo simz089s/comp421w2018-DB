62a3377a002bd1823bc7ddff567788539fffb7da
Simon Zheng <simz97@outlook.com>
2018-03-06 23:07:30 -0500

    Stop crashing Eclipse

--- hw2/Script.sql
+++ hw2/Script.sql
@@ -1,5 +1,41 @@
+-- 1.
+ SELECT
+	C.ccode,
+	C.credits
+FROM
+	course C
+WHERE
+	(
+		C.credits = 3
+		OR C.credits = 1
+	)
+	AND C.dept = 'computer science'
+ORDER BY
+	C.credits DESC,
+	C.ccode ASC;
+
+-- 2.
+ SELECT
+	C.ccode,
+	C.credits
+FROM
+	course C
+WHERE
+	C.dept = 'computer science'
+	AND C.ccode IN(
+		SELECT
+			CO.ccode
+		FROM
+			courseoffer CO
+		WHERE
+			CO.term = 'winter 2018'
+	)
+ORDER BY
+	C.ccode;
+
 SELECT
-	S.rating, AVG(S.age)
+	S.rating,
+	AVG( S.age )
 FROM
 	skaters S
 WHERE
@@ -7,6 +43,4 @@ WHERE
 GROUP BY
 	S.rating
 ORDER BY
-	S.rating
-;
-
+	S.rating DESC;
