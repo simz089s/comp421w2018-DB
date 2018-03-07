d6453c82588e45a769e99dbef869274c60f36b79
Simon Zheng <simz97@outlook.com>
2018-03-07 00:43:50 -0500

    HW2 4 questions

--- hw2/Script.sql
+++ hw2/Script.sql
@@ -1,41 +1,58 @@
 -- 1.
- SELECT
-	C.ccode,
-	C.credits
+SELECT
+	C.ccode, C.credits
 FROM
 	course C
 WHERE
-	(
-		C.credits = 3
-		OR C.credits = 1
-	)
+	( C.credits = 3 OR C.credits = 1 )
 	AND C.dept = 'computer science'
 ORDER BY
-	C.credits DESC,
-	C.ccode ASC;
+	C.credits DESC, C.ccode ASC
+;
 
 -- 2.
- SELECT
-	C.ccode,
-	C.credits
+SELECT
+	DISTINCT C.ccode, C.credits
+FROM
+	course C, courseoffer CO
+WHERE
+	C.dept = 'computer science'
+	AND C.ccode = CO.ccode
+	AND CO.term = 'winter 2018'
+ORDER BY
+	C.ccode
+;
+
+-- 3.
+SELECT
+	DISTINCT C.ccode, C.credits
+FROM
+	course C JOIN courseoffer CO ON C.ccode = CO.ccode
+WHERE
+	C.dept = 'computer science'
+	AND CO.term = 'winter 2018'
+ORDER BY
+	C.ccode
+;
+
+-- 4.
+SELECT
+	C.ccode, C.credits
 FROM
 	course C
 WHERE
 	C.dept = 'computer science'
 	AND C.ccode IN(
-		SELECT
-			CO.ccode
-		FROM
-			courseoffer CO
-		WHERE
-			CO.term = 'winter 2018'
-	)
+		SELECT CO.ccode
+		FROM courseoffer CO
+		WHERE CO.term = 'winter 2018' )
 ORDER BY
-	C.ccode;
+	C.ccode
+;
 
+-- .
 SELECT
-	S.rating,
-	AVG( S.age )
+	S.rating, AVG( S.age )
 FROM
 	skaters S
 WHERE
@@ -43,4 +60,6 @@ WHERE
 GROUP BY
 	S.rating
 ORDER BY
-	S.rating DESC;
+	S.rating DESC
+;
+
